00a52d09fe6ff642cd335718ceda2c5f4e68d11c
Chetan Oli <chetanoli@Chetans-MacBook-Pro.local>
2023-10-06 15:42:07 +0545

    React modal code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [show, setShow] = useState(false);
+  const handleShow = () => {
+    setShow((prevVal) => !prevVal);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <button onClick={handleShow}>{show ? "hide" : "show"} modal</button>
+      <Modal show={show} title={"Modal title"} onClose={handleShow}>
+        <h1>This is modal's body</h1>
+      </Modal>
     </div>
   );
 }
+
+const Modal = ({ show, onClose, title, children }) => {
+  return (
+    show && (
+      <>
+        <div className="modal-backdrop" onClick={onClose}></div>
+        <div className={`modal-wrapper ${show ? "active" : ""}`}>
+          <div className="modal-header">
+            <div className="modal-title"> {title}</div>
+            <span className="modal-close" onClick={onClose}>
+              x
+            </span>
+          </div>
+          <div className="modal-body">{children}</div>
+        </div>
+      </>
+    )
+  );
+};
 
 export default App;
